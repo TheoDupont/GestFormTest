0697b48653841d14a7bc6f8fc48e0a5769f6ac0c
Theo Dupont <theo.dupont@akka.eu>
2023-01-05 17:49:24 +0100

    Filtering arguments

--- app/Program.cs
+++ app/Program.cs
@@ -5,10 +5,31 @@ namespace GestFormTest
     class Program
     {
         static void Main(string[] args){
-
+            
             Console.WriteLine("Hello, World!");
+
+            for(int i=0; i<args.Length; ++i){
+
+                int number;
+
+                //Filtering args and exiting if requirements not fullfilled
+                //Args are numbers
+                if(!int.TryParse(args[i], out number)){
+                    Console.WriteLine("One of your arguments is not a number, please enter only numeric arguments.");
+                    Console.WriteLine("Argument recieved: " + args[i]);
+                    return;
+                }
+                //Args are between -1000 and 1000
+                if(number > 1000 || number < -1000){
+                    Console.WriteLine("One of your arguments is invalid. Please enter a number between -1000 and 1000 included");
+                    Console.WriteLine("Argument recieved: " + args[i]);
+                    return;
+                }
+                
+                Console.WriteLine(args[i]);
+            }
         }
 
-        
+
     }
 }
